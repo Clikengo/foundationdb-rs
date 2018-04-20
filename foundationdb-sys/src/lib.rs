b27457066d73f364dd23c98b39d186e0e4dee997
Benjamin Fry <benjaminfry@me.com>
2018-04-19 22:57:42 -0700

    initial binding generation working

--- foundationdb-sys/src/lib.rs
+++ foundationdb-sys/src/lib.rs
@@ -1,1 +1,15 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
 
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_api_version() {
+        assert_eq!(FDB_API_VERSION, 510);
+    }
+}
---
+++ foundationdb-sys/wrapper.h
@@ -0,0 +1,4 @@
+#define FDB_API_VERSION 510
+
+// FIXME: pkg-config didn't know how to find foundationdb
+#include "/usr/local/include/foundationdb/fdb_c.h"
