b6b40ea745a6c8f7f5c257cdd22cec2a3bccf9e5
Benjamin Fry <benjaminfry@me.com>
2018-04-21 16:10:45 -0700

    fix the travis install scripts

--- scripts/install_foundationdb_macos.sh
+++ scripts/install_foundationdb_macos.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -e
 
-curl -O https://www.foundationdb.org/downloads/5.1.5/ubuntu/installers/foundationdb-clients_5.1.5-1_amd64.deb
-curl -O https://www.foundationdb.org/downloads/5.1.5/ubuntu/installers/foundationdb-server_5.1.5-1_amd64.deb
+curl -O https://www.foundationdb.org/downloads/5.1.5/macOS/installers/FoundationDB-5.1.5.pkg
 
-sudo dpkg -i foundationdb-clients_5.1.5-1_amd64.deb
-sudo dpkg -i foundationdb-server_5.1.5-1_amd64.deb
+sudo installer -pkg FoundationDB-5.1.5.pkg -target /
