fa02b54d19895d0ba30cf6a3d54dbba319440a41
Benjamin Fry <benjaminfry@me.com>
2018-04-22 15:30:53 -0700

    add library level documentation and examples

--- foundationdb/src/network.rs
+++ foundationdb/src/network.rs
@@ -1,3 +1,10 @@
+// Copyright 2018 foundationdb-rs developers, https://github.com/bluejekyll/foundationdb-rs/graphs/contributors
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
 use std;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::thread;
@@ -37,6 +44,7 @@ impl Network {
 
     /// Wait for run to have started
     pub fn wait(&self) {
+        // TODO: rather than a hot loop, consider a condvar here...
         while !HAS_BEEN_RUN.load(Ordering::Acquire) {
             thread::yield_now();
         }
