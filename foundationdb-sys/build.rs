8b97139edf5fa4f052411292cdc6fdec50b6758a
Benjamin Fry <benjaminfry@me.com>
2018-04-22 19:47:20 -0700

    track bindings.rs in Git

--- foundationdb-sys/build.rs
+++ foundationdb-sys/build.rs
@@ -10,25 +10,30 @@ const INCLUDE_PATH: &str = "-I/usr/include/foundationdb/";
 const INCLUDE_PATH: &str = "-I/usr/local/include/foundationdb/";
 
 fn main() {
-    // Tell cargo to tell rustc to link the system bzip2
-    // shared library.
+    // Link against fdb_c
     println!("cargo:rustc-link-lib=fdb_c");
 
-    // The bindgen::Builder is the main entry point
-    // to bindgen, and lets you build up options for
-    // the resulting bindings.
-    let bindings = bindgen::Builder::default().clang_arg(INCLUDE_PATH)
+    if env::var_os("BINDGEN").is_some() {
+        // The bindgen::Builder is the main entry point
+        // to bindgen, and lets you build up options for
+        // the resulting bindings.
+        let bindings = bindgen::Builder::default()
+        // TODO: there must be a way to get foundationdb from pkg-config...
+        .clang_arg(INCLUDE_PATH)
         // The input header we would like to generate
         // bindings for.
         .header("wrapper.h")
+        .generate_comments(true)
         // Finish the builder and generate the bindings.
         .generate()
         // Unwrap the Result and panic on failure.
-        .expect("Unable to generate bindings");
+        .expect("Unable to generate FoundationDB bindings");
 
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+        // Write the bindings to the $OUT_DIR/bindings.rs file.
+        // let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+        let out_path = PathBuf::from("src");
+        bindings
+            .write_to_file(out_path.join("bindings.rs"))
+            .expect("Couldn't write bindings!");
+    }
 }
