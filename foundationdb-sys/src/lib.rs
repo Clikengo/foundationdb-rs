76db3a672878e3977986f1126a8e250ca0dc03f9
Austin Seipp <aseipp@pobox.com>
2018-11-16 06:14:39 -0600

    core: auto-generate all code at build time
    This moves the core codebase to generate all code related to
    FoundationDB at compile time, rather than build time. This gives
    more flexible control for clients who want to use specific server
    versions.
    
    Notably, foundationdb-sys now allows the user to control the
    FDB_API_VERSION setting when calling bindgen. This is exposed
    as a set of Cargo features which end-users can pick out: fdb-5_1,
    fdb-5_2, and fdb-6_0. This means that foundationdb-sys no longer
    has to ship the wrapper.h file, nor a pre-computed bindings.rs file.
    
    Next, it also makes foundationdb-gen a build-time dependency of the
    main foundationdb library, and it generates all of the options.rs
    module at compile time -- this is because it must be generated based on
    fdb.options, which is luckily shipped with all client libraries. Doing
    this required moving the generator into a library.
    
    foundationdb-gen was also tweaked slightly to support FDB 5.2, which
    introduced a few new special-cases in the option generation code
    (Notably, having to rewrite 'AppendIfFit' to the proper name,
    'AppendIfFits').
    
    With this in place, I can compile this library on 64-bit Windows for
    FoundationDB 5.2 with no modifications, automatically.
    
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- foundationdb-sys/src/lib.rs
+++ foundationdb-sys/src/lib.rs
@@ -2,14 +2,9 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-include!("bindings.rs");
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_api_version() {
-        assert_eq!(FDB_API_VERSION, 510);
-    }
 }
---
+++ foundationdb/build.rs
@@ -0,0 +1,17 @@
+extern crate foundationdb_gen;
+
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::PathBuf;
+
+fn main() {
+    let out_path = PathBuf::from(env::var("OUT_DIR").expect("OUT_DIR is undefined!"));
+    let options_file = out_path.join("options.rs");
+    let options = foundationdb_gen::emit().expect("couldn't emit options.rs code!");
+
+    File::create(options_file)
+        .expect("couldn't create options.rs!")
+        .write_all(options.as_bytes())
+        .expect("couldn't write options.rs!");
+}
