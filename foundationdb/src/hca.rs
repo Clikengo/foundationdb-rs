b54fb2bae8c570f6bd936255d4456bda3c1de546
Paul Hemberger <paul.hemberger@gmail.com>
2019-05-30 23:29:29 -0400

    working tests

--- foundationdb/src/hca.rs
+++ foundationdb/src/hca.rs
@@ -17,13 +17,14 @@ use futures::stream::Stream;
 use rand::Rng;
 
 use error::Error;
+use keyselector::KeySelector;
 use options::{ConflictRangeType, MutationType, TransactionOption};
 use subspace::Subspace;
 use transaction::{RangeOptionBuilder, Transaction};
 use tuple::Element;
 
 lazy_static! {
-  static ref LOCK: Mutex<i32> = Mutex::new(0);
+    static ref LOCK: Mutex<i32> = Mutex::new(0);
 }
 
 const ONE_BYTES: &[u8] = &[0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
@@ -37,32 +38,38 @@ pub struct HighContentionAllocator {
 }
 
 impl HighContentionAllocator {
-    fn new(subspace: Subspace) -> HighContentionAllocator {
+    /// New HCA
+    pub fn new(subspace: Subspace) -> HighContentionAllocator {
         HighContentionAllocator {
             counters: subspace.subspace(0),
             recent: subspace.subspace(1),
         }
     }
 
-    fn allocate(
-        &self,
-        transaction: &mut Transaction,
-        subspace: &Subspace,
-    ) -> Result<Subspace, Error> {
+    /// Returns a byte string that
+    ///   1) has never and will never be returned by another call to this
+    ///      method on the same subspace
+    ///   2) is nearly as short as possible given the above
+    pub fn allocate(&self, transaction: &mut Transaction) -> Result<i64, Error> {
+        let (begin, end) = self.counters.range();
+
         loop {
-            let range_option = RangeOptionBuilder::from(self.counters.range())
+            let counters_begin = KeySelector::first_greater_or_equal(&begin);
+            let counters_end = KeySelector::first_greater_than(&end);
+            let range_option = RangeOptionBuilder::new(counters_begin, counters_end)
                 .reverse(true)
                 .limit(1)
                 .snapshot(true)
                 .build();
 
-            let kvs: Vec<i64> = transaction.get_ranges(range_option)
+            let kvs: Vec<i64> = transaction
+                .get_ranges(range_option)
                 .map_err(|(_, e)| e)
                 .fold(Vec::new(), move |mut out, range_result| {
                     let kvs = range_result.key_values();
 
                     for kv in kvs.as_ref() {
-                        if let Element::I64(counter) = self.counters.unpack(kv.key()).expect("unable to unpack counter key") {
+                        if let Element::I64(counter) = self.counters.unpack(kv.key())? {
                             out.push(counter);
                         }
                     }
@@ -72,7 +79,7 @@ impl HighContentionAllocator {
                 .wait()?;
 
             let mut start: i64 = 0;
-            let mut window: i64 = 0;
+            let mut window: i64;
 
             if kvs.len() == 1 {
                 start = kvs[0];
@@ -84,17 +91,25 @@ impl HighContentionAllocator {
                 let mutex_guard = LOCK.lock().unwrap();
 
                 if window_advanced {
-                    transaction.clear_range(self.counters.bytes(), self.counters.subspace(start).bytes());
+                    transaction
+                        .clear_range(self.counters.bytes(), self.counters.subspace(start).bytes());
                     transaction.set_option(TransactionOption::NextWriteNoWriteConflictRange)?;
-                    transaction.clear_range(self.recent.bytes(), self.recent.subspace(start).bytes());
+                    transaction
+                        .clear_range(self.recent.bytes(), self.recent.subspace(start).bytes());
                 }
 
                 let counters_subspace_with_start = self.counters.subspace(start);
 
                 // Increment the allocation count for the current window
-                transaction.atomic_op(counters_subspace_with_start.bytes(), ONE_BYTES, MutationType::Add);
+                transaction.atomic_op(
+                    counters_subspace_with_start.bytes(),
+                    ONE_BYTES,
+                    MutationType::Add,
+                );
 
-                let subspace_start_trx = transaction.get(counters_subspace_with_start.bytes(), true).wait()?;
+                let subspace_start_trx = transaction
+                    .get(counters_subspace_with_start.bytes(), true)
+                    .wait()?;
                 let count = byteorder::LittleEndian::read_i64(subspace_start_trx.value().unwrap());
 
                 drop(mutex_guard);
@@ -116,24 +131,28 @@ impl HighContentionAllocator {
                 // subsequent risk of conflict for this transaction.
                 let mut rng = rand::thread_rng();
 
-                let candidate = self.recent.subspace(rng.gen::<i64>() + start);
-                let candidate_subspace = candidate.bytes();
+                let candidate: i64 = rng.gen_range(0, window) + start;
+                let recent_subspace_for_candidate = self.recent.subspace(candidate);
+                let candidate_subspace = recent_subspace_for_candidate.bytes();
 
                 let mutex_guard = LOCK.lock().unwrap();
 
-                let range_option = RangeOptionBuilder::from(self.counters.range())
+                let counters_begin = KeySelector::first_greater_or_equal(&begin);
+                let counters_end = KeySelector::first_greater_than(&end);
+                let range_option = RangeOptionBuilder::new(counters_begin, counters_end)
                     .reverse(true)
                     .limit(1)
                     .snapshot(true)
                     .build();
 
-                let kvs: Vec<i64> = transaction.get_ranges(range_option)
+                let kvs: Vec<i64> = transaction
+                    .get_ranges(range_option)
                     .map_err(|(_, e)| e)
                     .fold(Vec::new(), move |mut out, range_result| {
                         let kvs = range_result.key_values();
 
                         for kv in kvs.as_ref() {
-                            if let Element::I64(counter) = self.counters.unpack(kv.key()).expect("unable to unpack counter key") {
+                            if let Element::I64(counter) = self.counters.unpack(kv.key())? {
                                 out.push(counter);
                             }
                         }
@@ -149,7 +168,7 @@ impl HighContentionAllocator {
 
                 drop(mutex_guard);
 
-                if kvs.len() > 0 {
+                if !kvs.is_empty() {
                     let current_start = kvs[0];
 
                     if current_start > start {
@@ -160,14 +179,14 @@ impl HighContentionAllocator {
                 let candidate_value = candidate_value_trx.wait()?;
 
                 match candidate_value.value() {
-                    Some(x) => {
-                        if x.len() == 0 {
-                            transaction.add_conflict_range(candidate_subspace, candidate_subspace, ConflictRangeType::Write)?;
-                            return Ok(self.counters.subspace(candidate_subspace));
-                        }
-                    }
+                    Some(_) => (),
                     None => {
-                        continue;
+                        transaction.add_conflict_range(
+                            candidate_subspace,
+                            candidate_subspace,
+                            ConflictRangeType::Write,
+                        )?;
+                        return Ok::<_, Error>(candidate);
                     }
                 };
             }
@@ -180,12 +199,10 @@ impl HighContentionAllocator {
         // can't be too small.  So start small and scale up.  We don't want this to
         // ever get *too* big because we have to store about window_size/2 recent
         // items.
-        if start < 255 {
-            return 64;
+        match start {
+            _ if start < 255 => 64,
+            _ if start < 65535 => 1024,
+            _ => 8192,
         }
-        if start < 65535 {
-            return 1024;
-        }
-        return 8192;
     }
 }
