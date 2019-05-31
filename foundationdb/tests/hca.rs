67507d16598fe5a0884751947da5fff06d4a80db
Paul Hemberger <paul.hemberger@gmail.com>
2019-05-30 23:29:29 -0400

    working tests

--- foundationdb/tests/hca.rs
+++ foundationdb/tests/hca.rs
@@ -11,74 +11,107 @@ extern crate futures;
 extern crate lazy_static;
 
 use std::collections::HashSet;
+use std::iter::FromIterator;
 
 use futures::future::*;
-use futures::prelude::*;
 
 use foundationdb::error::Error;
 use foundationdb::hca::HighContentionAllocator;
-use foundationdb::tuple::{Element, Tuple};
 use foundationdb::*;
 
 mod common;
 
 #[test]
-fn test_allocate() {
-    use foundationdb::keyselector::KeySelector;
-
+fn test_hca_many_sequential_allocations() {
     common::setup_static();
-    const N: usize = 100;
-    const KEY: &[u8] = b"h";
+    const N: usize = 6000;
+    const KEY: &[u8] = b"test-hca-allocate";
 
     let fut = Cluster::new(foundationdb::default_config_path())
         .and_then(|cluster| cluster.create_database())
-        //        .and_then(|db| result(db.create_trx()))
         .and_then(|db: Database| {
+            let cleared_range = db
+                .transact(move |tx| {
+                    tx.clear_subspace_range(Subspace::from_bytes(KEY));
+                    futures::future::result(Ok::<(), failure::Error>(()))
+                })
+                .wait();
+
+            cleared_range.expect("unable to clear hca test range");
+
             let hca = HighContentionAllocator::new(Subspace::from_bytes(KEY));
 
-            db.transact(move |tx| {
-                tx.clear_range(KEY, KEY);
-                futures::future::result(Ok::<(), failure::Error>(()))
-            })
-            .wait();
-
             let mut all_ints = Vec::new();
-
-            //            let mut tx: Transaction = db.create_trx()?;
-            //            tx.clear_subspace_range(Subspace::from_bytes(KEY));
-            //            tx.commit().wait()?;
 
             for _ in 0..N {
                 let mut tx: Transaction = db.create_trx()?;
 
                 let next_int: i64 = hca.allocate(&mut tx)?;
-                println!("next: {:?}", next_int);
                 all_ints.push(next_int);
 
-                tx.commit().wait();
+                tx.commit().wait()?;
             }
 
             Ok::<_, Error>(all_ints)
         });
-    //        .and_then(|mut trx| {
-    //            println!("starting to run");
-    //            let hca = HighContentionAllocator::new(Subspace::from_bytes(KEY));
-    //
-    //            trx.clear_range(KEY, KEY);
-    //
-    //            let mut all_ints = Vec::new();
-    //
-    //            for _ in 0..N {
-    //
-    //                let next_int : i64 = hca.allocate(&mut trx)?;
-    //                all_ints.push(next_int);
-    ////                println!("next: {:?}", next_int);
-    //            }
-    //
-    //            Ok::<_, Error>(all_ints)
-    //        });
 
-    println!("running test");
     let all_ints: Vec<i64> = fut.wait().expect("failed to run");
-    println!("ran test {:?}", all_ints);
+    check_hca_result_uniqueness(&all_ints);
+
+    eprintln!("ran test {:?}", all_ints);
+}
+
+#[test]
+fn test_hca_concurrent_allocations() {
+    common::setup_static();
+    const N: usize = 1000;
+    const KEY: &[u8] = b"test-hca-allocate-concurrent";
+
+    let fut = Cluster::new(foundationdb::default_config_path())
+        .and_then(|cluster| cluster.create_database())
+        .and_then(|db: Database| {
+            let cleared_range = db
+                .transact(move |tx| {
+                    tx.clear_subspace_range(Subspace::from_bytes(KEY));
+                    futures::future::result(Ok::<(), failure::Error>(()))
+                })
+                .wait();
+
+            cleared_range.expect("unable to clear hca test range");
+
+            let mut futures = Vec::new();
+            let mut all_ints: Vec<i64> = Vec::new();
+
+            for _ in 0..N {
+                let f = db.transact(move |mut tx| {
+                    HighContentionAllocator::new(Subspace::from_bytes(KEY)).allocate(&mut tx)
+                });
+
+                futures.push(f);
+            }
+
+            for allocation in futures {
+                let i = allocation.wait().expect("unable to get allocation");
+                all_ints.push(i);
+            }
+
+            Ok::<_, Error>(all_ints)
+        });
+
+    let all_ints: Vec<i64> = fut.wait().expect("failed to run");
+    check_hca_result_uniqueness(&all_ints);
+
+    eprintln!("ran test {:?}", all_ints);
+}
+
+fn check_hca_result_uniqueness(results: &Vec<i64>) {
+    let result_set: HashSet<i64> = HashSet::from_iter(results.clone());
+
+    if results.len() != result_set.len() {
+        panic!(
+            "Set size does not much, got duplicates from HCA. Set: {:?}, List: {:?}",
+            result_set.len(),
+            results.len(),
+        );
+    }
 }
