a86f82bc593ea990094513e8b0b74ce8ce615c70
Vincent Rouillé <vincent@speedy37.fr>
2019-11-18 17:42:53 +0100

    restore abort future test

---
+++ foundationdb/tests/future.rs
@@ -0,0 +1,57 @@
+use foundationdb::*;
+use futures::prelude::*;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+
+mod common;
+
+struct AbortingFuture<F> {
+    inner: F,
+    polled: bool,
+}
+
+impl<T> Future for AbortingFuture<T>
+where
+    T: Future + Unpin,
+{
+    type Output = FdbResult<()>;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<FdbResult<()>> {
+        // poll once only
+        if !self.polled {
+            self.polled = true;
+            match Pin::new(&mut self.as_mut().inner).poll(cx) {
+                Poll::Pending => (),
+                _ => panic!("pending was expected"),
+            }
+        }
+
+        Poll::Ready(Ok(()))
+    }
+}
+
+#[test]
+// dropping a future while it's in the pending state should not crash
+fn test_future_discard() {
+    common::boot();
+    futures::executor::block_on(test_future_discard_async()).expect("failed to run");
+}
+async fn test_future_discard_async() -> FdbResult<()> {
+    let db = common::database().await?;
+    for _i in 0..=1000 {
+        db.transact(
+            (),
+            |trx, ()| {
+                AbortingFuture {
+                    inner: trx.get(b"key", false),
+                    polled: false,
+                }
+                .boxed_local()
+            },
+            TransactOption::default(),
+        )
+        .await?;
+    }
+
+    Ok(())
+}
