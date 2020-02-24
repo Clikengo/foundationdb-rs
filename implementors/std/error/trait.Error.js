fc2c6a2d5b7748f94bd19f1e12380832633faceb
Speedy37 <Speedy37@users.noreply.github.com>
2020-02-24 21:15:43 +0000

    Automated deployment: Mon Feb 24 21:15:43 UTC 2020 6764a8175e89ebd8c3335208b6624a4018a8c8f5

---
+++ implementors/std/error/trait.Error.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["foundationdb"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"foundationdb/struct.FdbError.html\" title=\"struct foundationdb::FdbError\">FdbError</a>","synthetic":false,"types":["foundationdb::error::FdbError"]}];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
