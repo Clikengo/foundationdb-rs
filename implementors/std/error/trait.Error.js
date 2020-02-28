ee8be5b88ee6d26928a6a99a5148fcc8cd44898b
Speedy37 <Speedy37@users.noreply.github.com>
2020-02-28 18:19:03 +0000

    Automated deployment: Fri Feb 28 18:19:03 UTC 2020 2d8faa0b5f62c623aa29bbec013d529acb845553

--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["foundationdb"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"foundationdb/struct.FdbError.html\" title=\"struct foundationdb::FdbError\">FdbError</a>","synthetic":false,"types":["foundationdb::error::FdbError"]}];
+implementors["foundationdb"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"foundationdb/struct.FdbError.html\" title=\"struct foundationdb::FdbError\">FdbError</a>","synthetic":false,"types":["foundationdb::error::FdbError"]},{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"foundationdb/tuple/enum.PackError.html\" title=\"enum foundationdb::tuple::PackError\">PackError</a>","synthetic":false,"types":["foundationdb::tuple::PackError"]}];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
