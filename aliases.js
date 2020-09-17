8de1ad2750288cee03e391ed051942aa39afc1bd
Speedy37 <Speedy37@users.noreply.github.com>
2020-09-17 20:26:17 +0000

    Automated deployment: Thu Sep 17 20:26:17 UTC 2020 a5eee483bb6ebb9cf7a0bbe1dc80a20ff01cfe20

--- aliases.js
+++
@@ -1,8 +0,0 @@
-var ALIASES = {};
-ALIASES["bindingtester"] = {};
-ALIASES["fdb_bench"] = {};
-ALIASES["foundationdb"] = {};
-ALIASES["foundationdb_bench"] = {};
-ALIASES["foundationdb_gen"] = {};
-ALIASES["foundationdb_options_gen"] = {};
-ALIASES["foundationdb_sys"] = {};
