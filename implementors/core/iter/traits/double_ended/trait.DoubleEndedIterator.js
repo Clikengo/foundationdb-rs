707b5f5f8a8c81e667d6c907c1b747305074e019
Speedy37 <Speedy37@users.noreply.github.com>
2020-10-18 15:14:50 +0000

    Automated deployment: Sun Oct 18 15:14:50 UTC 2020 52db8a43148262d3a0b40fe7f057663e5eb50ad3

--- implementors/core/iter/traits/double_ended/trait.DoubleEndedIterator.js
+++ implementors/core/iter/traits/double_ended/trait.DoubleEndedIterator.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["foundationdb"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/double_ended/trait.DoubleEndedIterator.html\" title=\"trait core::iter::traits::double_ended::DoubleEndedIterator\">DoubleEndedIterator</a> for <a class=\"struct\" href=\"foundationdb/future/struct.FdbValuesIter.html\" title=\"struct foundationdb::future::FdbValuesIter\">FdbValuesIter</a>","synthetic":false,"types":["foundationdb::future::FdbValuesIter"]}];
+implementors["foundationdb"] = [{"text":"impl DoubleEndedIterator for FdbValuesIter","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/structopt/trait.StructOptInternal.js
+++ implementors/structopt/trait.StructOptInternal.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["fdb_bench"] = [{"text":"impl StructOptInternal for <a class=\"struct\" href=\"fdb_bench/struct.Opt.html\" title=\"struct fdb_bench::Opt\">Opt</a>","synthetic":false,"types":["fdb_bench::Opt"]}];
+implementors["fdb_bench"] = [{"text":"impl StructOptInternal for Opt","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
